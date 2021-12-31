bfc7249b6015f8747abc61eb637231921f21b390
ProjectFrank <frank.wangtao@gmail.com>
2021-12-30 22:14:15 -0500

    Use reitit for routing
    Replace compojure with reitit for routing.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [aero "1.1.6"]
-                 [compojure "1.6.1"]
+                 [metosin/reitit-ring "0.5.15"]
                  [prismatic/schema "1.2.0"]
                  [ring "1.9.4"]
                  [ring/ring-defaults "0.3.2"]
--- src/version_tracker/handler.clj
+++ src/version_tracker/handler.clj
@@ -1,11 +1,17 @@
 (ns version-tracker.handler
-  (:require [compojure.core :refer :all]
-            [compojure.route :as route]
+  (:require [reitit.ring :as router]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+(defn handle-hello [_]
+  {:status 200 :body "Hello World" :headers {"Content-Type" "text/html"}})
+
+(def router (router/router
+             ["/" {:get handle-hello}]))
+
+(def default-handler (router/create-default-handler
+                      {:not-found (constantly {:status 404 :body "Not Found"})}))
+
+(def handler (router/ring-handler router default-handler))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults handler site-defaults))
